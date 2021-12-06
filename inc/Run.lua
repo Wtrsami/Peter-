bd1bdac4118f52a05660300c4c13bae7725ff580
Wtrsami <92092807+Wtrsami@users.noreply.github.com>
2021-12-07 00:19:32 +0300

    Add files via upload

--- inc/Run.lua
+++ inc/Run.lua
@@ -534,13 +534,13 @@ local Text = [[‏‌‌‏‌‌‌‌‏ ‏‌‌‏‌‌‌‌‏ • ‌�
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text="م1",callback_data="/m1:"..user_id},{text="م2",callback_data="/m2:"..user_id}},
-{{text="م3",callback_data="/m3:"..user_id},{text="م4",callback_data="/m4:"..user_id}},
+{{text="م3",callback_data="/m3:"..user_id},{text="م4",callback_data="/m4:"..user_id},
 {text="م5",callback_data="/m5:"..user_id}},
 {{text="اوامر التحميل",callback_data="/music:"..user_id}},
 } 
 return https.request("https://api.telegram.org/bot"..Token..'/editMessageText?chat_id='..Chat_Id2..'&message_id='..Msg_Id2..'&text=' .. URL.escape(Text).."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
 end
-}
+
 if DataText and DataText:match('(.*)/help1') then
 local Teext =[[
 اضغطي الزر لتغيير الافتار
